1b754355def8eff820206164527c29efff2fd0f7
Fergus Currie <ferguscurrie@Ferguss-MacBook-Air-2.local>
2024-02-25 15:08:13 +1000

    feat: basic scheduler

--- spaced/spaced_app/views.py
+++ spaced/spaced_app/views.py
@@ -21,7 +21,8 @@ def parse_content(content) -> (str, str):
     question, answer = content.split("<br><br>")
     question, answer = clean_latex(question), clean_latex(answer)
     if question != "<br/>" and answer != "<br/>":
-        Card.objects.create(question, answer, created=timezone.now())
+        print(question, answer)
+        Card.objects.create(question=question, answer=answer, created=timezone.now())
     
 
 def add_cards(request):
@@ -30,6 +31,41 @@ def add_cards(request):
         parse_content(content)
     return render(request, "add_card.html", {})
 
+def sort_cards_by_ease(cards, reviews):
+
+    to_review = []
+    to_review_weight = []
+    for card in cards: 
+        card_reviews = sorted([r for r in reviews if r.card == card], key=lambda review: review.date)
+        if len(card_reviews) ==0:
+            to_review.append(card)
+            to_review_weight.append(0)
+            continue 
+        interval = 0 
+        for review in card_reviews:
+            if review.ease == 3:
+                interval = 0
+            if review.ease == 2:
+                interval += 1
+            if review.ease == 1:
+                if interval == 0:
+                    interval = 1
+                interval *= 1.5 
+        days_since_first_review = (timezone.now() - card_reviews[0].date).seconds / (60 * 24)
+        
+        remaining_interval = days_since_first_review - interval 
+        if remaining_interval > 0:
+            # add to review stack, weighted by diff 
+            to_review.append(card)
+            to_review_weight.append(remaining_interval)
+        else:
+            # else pass 
+            pass
+
+    sorted_pairs = sorted(zip(to_review, to_review_weight), key=lambda x: x[1], reverse=True)
+    sorted_to_review = [pair[0] for pair in sorted_pairs]
+    return sorted_to_review
+
 
 # Create your views here.
 def review_cards(request):
@@ -37,7 +73,7 @@ def review_cards(request):
     # Retrieve the card index from URL parameter, defaulting to 0
     card_index = request.GET.get('index', 0)
     difficulty = request.GET.get('difficulty', None)
-    print(card_index, difficulty)
+    
     try:
         card_index = int(card_index)
     except ValueError:
@@ -45,7 +81,10 @@ def review_cards(request):
     
     # Get all cards (or a subset if you have too many)
     cards = list(Card.objects.all())
+    reviews = list(Review.objects.all())
     
+    cards = sort_cards_by_ease(cards, reviews)
+
     # Ensure the index is within bounds
     if card_index < 0:
         card_index = 0
