8b8d52ec546031320e756081fbcf7ccf5f9ab3ee
Fergus Currie <ferguscurrie@Ferguss-MacBook-Air-2.local>
2024-03-02 13:17:21 +1000

    feat: poetry package

--- spaced/spaced_app/views.py
+++ spaced/spaced_app/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from django.urls import reverse
 from bs4 import BeautifulSoup
-import os 
+from sm2_algorithm import sm2_algorithm
 from .import_from_obsidian import do_generate
-import datetime 
+
 
 
 EASE_FACTOR_INIT = 2.5 
@@ -43,39 +43,7 @@ def add_cards(request):
             Card.objects.create(question=question, answer=answer, created=timezone.now())
     return render(request, "add_card.html", {})
 
-def sm2_algorithm(q, n, EF, I):
-    """
-    Implements the SM-2 algorithm used in spaced repetition systems.
 
-    Parameters:
-    q (int): User grade (quality response to the flashcard, typically from 0 to 5)
-    n (int): Repetition number for the current flashcard
-    EF (float): Current easiness factor of the flashcard
-    I (int): Current interval before the flashcard is to be reviewed again
-
-    Returns:
-    tuple: Updated values of n, EF, and I
-    """
-    
-    # Check if the response was correct
-    if q >= 3:
-        if n == 0:
-            I = 1
-        elif n == 1:
-            I = 6
-        else:
-            I = round(I * EF)
-        n += 1
-    else:  # Incorrect response
-        n = 0
-        I = 1
-
-    # Update the easiness factor
-    EF = EF + (0.1 - (5 - q) * (0.08 + (5 - q) * 0.02))
-    if EF < 1.3:
-        EF = 1.3
-
-    return n, EF, I
 
 # Create your views here.
 def review_cards(request):
